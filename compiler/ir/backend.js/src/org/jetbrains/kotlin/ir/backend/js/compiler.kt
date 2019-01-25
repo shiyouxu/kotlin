9a574cd60cf3395cfc9538b6df65f72d20993e42
Roman Artemev <roman.artemev@jetbrains.com>
2019-01-25 20:17:07 +0300

    merge metadata [de]serialization

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/compiler.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/compiler.kt
@@ -7,17 +7,27 @@ package org.jetbrains.kotlin.ir.backend.js
 
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.backend.common.CompilerPhaseManager
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.incremental.components.LookupTracker
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.metadata.*
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.js.analyze.TopDownAnalyzerFacadeForJS
+import org.jetbrains.kotlin.js.resolve.JsPlatform
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.progress.ProgressIndicatorAndCompilationCanceledStatus
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi2ir.Psi2IrTranslator
+import org.jetbrains.kotlin.resolve.CompilerDeserializationConfiguration
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
+import org.jetbrains.kotlin.utils.JsMetadataVersion
+import org.jetbrains.kotlin.utils.KotlinJavascriptMetadataUtils
+import java.io.File
 
 data class Result(val moduleDescriptor: ModuleDescriptor, val generatedCode: String, val moduleFragment: IrModuleFragment)
 
@@ -70,6 +80,80 @@ fun compile(
 //        val serializedData = serializer.serializeModule(analysisResult.moduleDescriptor, serializedIr)
 //        buildLibrary(serializedData)
 //
+
+//        val declarationTable = DeclarationTable(moduleFragment.irBuiltins, DescriptorTable())
+//        val serializedIr = IrModuleSerializer(context, declarationTable/*, onlyForInlines = false*/).serializedIrModule(moduleFragment)
+        val serializer = JsKlibMetadataSerializationUtil
+//        val serializedData = serializer.serializeModule(analysisResult.moduleDescriptor, serializedIr)
+        val moduleDescription = JsKlibMetadataModuleDescriptor(
+            name = configuration.get(CommonConfigurationKeys.MODULE_NAME) as String,
+            data = moduleFragment.descriptor,
+            kind = ModuleKind.UMD,
+            imported = dependencies.map { it.name.asString() }
+        )
+        var index = 0L
+        val serializedData = serializer.serializeMetadata(
+            psi2IrContext.bindingContext,
+            moduleDescription,
+            configuration.get(CommonConfigurationKeys.LANGUAGE_VERSION_SETTINGS)!!,
+            configuration.get(CommonConfigurationKeys.METADATA_VERSION)  as? JsMetadataVersion
+                ?: JsMetadataVersion.INSTANCE
+        ) {
+//            val index = declarationTable.descriptorTable.get(it)
+//            index?.let { newDescriptorUniqId(it) }
+            JsKlibMetadataProtoBuf.DescriptorUniqId.newBuilder().setIndex(index++).build()
+
+        }
+//        buildLibrary(serializedData)
+//
+
+        val stdKlibDir = File("js/js.translator/testData/out/klibs/runtime/").also {
+            it.deleteRecursively()
+            it.mkdirs()
+        }
+//
+//        val moduleFile = File(stdKlibDir, "module.kji")
+//        moduleFile.writeBytes(serializedIr.module)
+//
+//        for ((id, data) in serializedIr.declarations) {
+//            val file = File(stdKlibDir, "${id.index}${if (id.isLocal) "L" else "G"}.kjd")
+//            file.writeBytes(data)
+//        }
+//
+//
+//        val debugFile = File(stdKlibDir, "debug.txt")
+//
+//        for ((id, data) in serializedIr.debugIndex) {
+//            debugFile.appendText(id.toString())
+//            debugFile.appendText(" --- ")
+//            debugFile.appendText(data)
+//            debugFile.appendText("\n")
+//        }
+
+        val metadata = File(stdKlibDir, "${project.name}${KotlinJavascriptMetadataUtils.META_JS_SUFFIX}").also {
+            it.writeText(serializedData.asString())
+        }
+
+//
+        val storageManager = LockBasedStorageManager("JsConfig")
+//        // CREATE NEW MODULE DESCRIPTOR HERE AND DESERIALIZE IT
+//
+        val metadatas = KotlinJavascriptMetadataUtils.loadMetadata(metadata)
+        val mt = metadatas.single()
+        val lookupTracker = configuration.get(CommonConfigurationKeys.LOOKUP_TRACKER, LookupTracker.DO_NOTHING)
+        val parts = serializer.readModuleAsProto(mt.body, mt.version)
+        val md = ModuleDescriptorImpl(
+            Name.special("<" + mt.moduleName + ">"), storageManager, JsPlatform.builtIns
+        )
+        val provider = createJsKlibMetadataPackageFragmentProvider(
+            storageManager, md, parts.header, parts.body, mt.version,
+            CompilerDeserializationConfiguration(configuration.languageVersionSettings),
+            lookupTracker
+        )
+
+        md.initialize(provider)
+        md.setDependencies(listOf(md, md.builtIns.builtInsModule))
+
         TODO("Implemenet IrSerialization")
     }
 
