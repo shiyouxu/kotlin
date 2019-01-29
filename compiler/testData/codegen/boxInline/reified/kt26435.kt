25e7a0cdfa8a093a8d85a4bad168dd802890561f
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-01-29 11:34:56 +0100

    Calculate properly return type for reified functions
     #KT-26435 Fixed

---
+++ compiler/testData/codegen/boxInline/reified/kt26435.kt
@@ -0,0 +1,38 @@
+// JVM_TARGET: 1.8
+// FILE: 1.kt
+package test
+
+enum class Id {
+    ID_A,
+    ID_B
+}
+
+sealed class Base(val id: Id)
+class A(id: Id) : Base(id)
+class B(id: Id) : Base(id)
+
+inline fun <reified T : Base> process(t: T, f: (T) -> Unit): T {
+    f(t)
+    return getSomeBaseObject(t.id) as? T ?: throw RuntimeException()
+}
+
+fun getSomeBaseObject(id: Id): Base = if (id == Id.ID_A) A(id) else B(id)
+
+// FILE: 2.kt
+
+import test.*
+
+fun doSth(base: Base): Base = when (base) {
+    is A -> process(base, f = ::doSomethingInCaseOfA)
+    is B -> process(base, f = ::doSomethingInCaseOfB)
+}
+
+fun doSomethingInCaseOfA(a: A) {}
+
+fun doSomethingInCaseOfB(b: B) {}
+
+fun box(): String {
+    val a = doSth(A(Id.ID_A))
+
+    return "OK"
+}
