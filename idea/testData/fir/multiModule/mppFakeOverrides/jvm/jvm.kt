b6dc8296c5effae933de284c1d66aa3fc4baeedd
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-31 15:45:43 +0300

    Initiating test for fake overrides

---
+++ idea/testData/fir/multiModule/mppFakeOverrides/jvm/jvm.kt
@@ -0,0 +1,7 @@
+actual open class A<T> {
+    actual open fun foo(arg: T) {}
+    open fun bar(arg: T): T = arg
+}
+class D : C() {
+    // Fake: override fun bar(arg: CharSequence): String = super.bar(arg)
+}
